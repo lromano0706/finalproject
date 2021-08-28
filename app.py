ef62c6d0bbd27cf830c4146697f8ce5fef17ab50
Jonathan <jondtran132@gmail.com>
2021-08-27 23:41:18 -0700

    connected postgres to flask

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request, jsonify
+import json
+import pandas as pd
 import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 import csv
-# from flask_cors import CORS
-from mlmodel import mlmodel
+# from mlmodel import mlmodel
 
+engine = create_engine("postgresql://root:postgres@mypostgresdb.c57smewha8wr.us-west-1.rds.amazonaws.com:5432/finalproject")
+conn = engine.connect()
 
-app = Flask(__name__, template_folder="templates")
-# CORS(app)
+app = Flask(__name__)
 
 @app.route("/")
 def index():   
     return render_template("index.html")
 
+# probably delete this later v
 @app.route("/api/v1.0/crime")
 def crime():
     with open("../finalproject/Data/crime_cities_coords.csv", "r") as f:
@@ -24,6 +31,21 @@ def crime():
 def estimate(valString):
     values = valString.split('+')
     return jsonify(round(mlmodel(values)[0],0))
+
+@app.route("/houses")
+def houses():
+    data = json.loads(pd.read_sql("SELECT * FROM clean_real_estate", conn).to_json(orient="records"))
+    return jsonify(data)
+
+@app.route("/schools")
+def schools():
+    data = json.loads(pd.read_sql("SELECT * FROM school_data", conn).to_json(orient="records"))
+    return jsonify(data)
+
+@app.route("/crimes")
+def crimes():
+    data = json.loads(pd.read_sql("SELECT * FROM crime_cities_coords", conn).to_json(orient="records"))
+    return jsonify(data)
 
 
 if __name__ == "__main__":
--- working.py
+++ working.py
@@ -16,32 +16,53 @@
 #     zeros += 1
 # print(zer)
 
-cities = ['Aliso Viejo','Anaheim','Brea','Buena Park','Costa Mesa',
-              'Cypress','Dana Point','Fountain Valley','Fullerton','Garden Grove',
-              'Huntington Beach','Irvine','La Habra','La Palma','Laguna Beach',
-              'Laguna Hills','Laguna Niguel','Lake Forest','Los Alamitos',
-              'Mission Viejo','Newport Beach','Orange','Placentia',
-              'Rancho Santa Margarita','San Clemente','San Juan Capistrano',
-              'Santa Ana','Seal Beach','Stanton','Tustin','Villa Park',
-              'Westminster','Yorba Linda']
-zips = ['90620','90621','90623','90630','90631','90680','90720','90740',
-            '92602','92603','92604','92606','92610','92612','92614','92618','92620',
-            '92624','92626','92627','92629','92630','92646','92647','92648','92649',
-            '92651','92653','92656','92660','92661','92663','92672','92673','92675',
-            '92677','92679','92683','92688','92691','92692','92701','92703','92704',
-            '92705','92706','92707','92708','92780','92782','92801','92802','92804',
-            '92805','92806','92807','92821','92823','92831','92832','92833','92835',
-            '92840','92841','92843','92844','92845','92861','92865','92866','92867',
-            '92868','92869','92870','92886','92887']
-schoolRatings = ['2.333','2.667','3.0','3.333','3.667','4.0','4.333','4.4',
-                      '4.429','4.6','4.667','4.75','4.833','5.0','5.25','5.333','5.5','5.667',
-                      '5.75','6.0','6.25','6.333','6.5','6.667','6.75','7.0','7.25','7.333',
-                      '7.667','7.75','8.0','8.2','8.25','8.333','8.5','8.667','8.75','9.0','9.333']
-for x in cities:
-    print(f"<option value='{x}'>{x}</option>")
+# cities = ['Aliso Viejo','Anaheim','Brea','Buena Park','Costa Mesa',
+#               'Cypress','Dana Point','Fountain Valley','Fullerton','Garden Grove',
+#               'Huntington Beach','Irvine','La Habra','La Palma','Laguna Beach',
+#               'Laguna Hills','Laguna Niguel','Lake Forest','Los Alamitos',
+#               'Mission Viejo','Newport Beach','Orange','Placentia',
+#               'Rancho Santa Margarita','San Clemente','San Juan Capistrano',
+#               'Santa Ana','Seal Beach','Stanton','Tustin','Villa Park',
+#               'Westminster','Yorba Linda']
+# zips = ['90620','90621','90623','90630','90631','90680','90720','90740',
+#             '92602','92603','92604','92606','92610','92612','92614','92618','92620',
+#             '92624','92626','92627','92629','92630','92646','92647','92648','92649',
+#             '92651','92653','92656','92660','92661','92663','92672','92673','92675',
+#             '92677','92679','92683','92688','92691','92692','92701','92703','92704',
+#             '92705','92706','92707','92708','92780','92782','92801','92802','92804',
+#             '92805','92806','92807','92821','92823','92831','92832','92833','92835',
+#             '92840','92841','92843','92844','92845','92861','92865','92866','92867',
+#             '92868','92869','92870','92886','92887']
+# schoolRatings = ['2.333','2.667','3.0','3.333','3.667','4.0','4.333','4.4',
+#                       '4.429','4.6','4.667','4.75','4.833','5.0','5.25','5.333','5.5','5.667',
+#                       '5.75','6.0','6.25','6.333','6.5','6.667','6.75','7.0','7.25','7.333',
+#                       '7.667','7.75','8.0','8.2','8.25','8.333','8.5','8.667','8.75','9.0','9.333']
+# for x in cities:
+#     print(f"<option value='{x}'>{x}</option>")
 
-for x in zips:
-    print(f"<option value='{x}'>{x}</option>")
+# for x in zips:
+#     print(f"<option value='{x}'>{x}</option>")
 
-for x in schoolRatings:
-    print(f"<option value='{x}'>{x}</option>")
+# for x in schoolRatings:
+#     print(f"<option value='{x}'>{x}</option>")
+
+
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+engine = create_engine("postgresql://root:postgres@mypostgresdb.c57smewha8wr.us-west-1.rds.amazonaws.com:5432/finalproject")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+from sqlalchemy import inspect
+inspector = inspect(engine)
+
+for table_name in inspector.get_table_names():
+   for column in inspector.get_columns(table_name):
+       print("Column: %s" % column['name'])
+print(inspector.get_table_names())
